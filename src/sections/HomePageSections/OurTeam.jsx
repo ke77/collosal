609b70af703c93ccab31eae80c09117563d2f9e8
Kelvin <agbakpeykelvin@gmail.com>
2024-11-21 21:26:37 +0000

    check

--- src/sections/HomePageSections/OurTeam.jsx
+++ src/sections/HomePageSections/OurTeam.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { HashLink as Link } from 'react-router-hash-link/dist/react-router-hash-link.cjs.production'
 import { SecondaryButton } from '../../components/Buttons'
-import ourTeamIllustration from '/images/illustrations/Team_Illustration.svg'
+import ourTeamIllustration from '/images/illustrations/Team_Illustration.svg';
 
 
 const OurTeam = () => {
